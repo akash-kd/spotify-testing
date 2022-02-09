9ce0847d4d80ac4248395aaea57dfeed62a45d21
Akash Kandasamy <akashram953@gmail.com>
2022-02-09 15:43:49 +0530

    added player

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import './App.css';
+import SpotifyPlayer from 'react-spotify-player';
+import SpotifyWebApi from 'spotify-web-api-node';
+
 function App() {
+  const size = {
+    width: '80%',
+    height: 80,
+  };
   return (
     <div className="App">
-
+    <SpotifyPlayer
+      uri="spotify:track:6ozxplTAjWO0BlUxN8ia0A"
+      size={size}
+    />
+      
     </div>
   );
 }
