4f0f8ae9edddc76199afb791032b60b0c7a97fde
Akash Kandasamy <akashram953@gmail.com>
2022-02-10 19:37:46 +0530

    model implementations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import SpotifyPlayer from 'react-spotify-player';
 import SpotifyWebApi from 'spotify-web-api-node';
+import { useHotkeys,isHotkeyPressed} from 'react-hotkeys-hook';
+import Modal from 'react-modal'
 import { useEffect, useState } from 'react';
+import { ModifierBitMasks } from '@blueprintjs/core/lib/esm/components/hotkeys/hotkeyParser';
 
 function App() {
   const [tracks,setTracks] = useState([])
   const [searchText,setSearchText] = useState("")
   const [track,setTrack] = useState("spotify:track:6ozxplTAjWO0BlUxN8ia0A")
+  const [modalOpen,setModalOpen] = useState(false);
 
   const size = {
     width: '50%',
@@ -18,7 +22,7 @@ function App() {
     clientSecret: 'de0201ebd0c1400894a08d4bb18621bf',
   });
 
-  let token = 'BQAV22tJSlmOwJ8pWjDxgFqnD5ZSFxu7ejLKoXxvxhnPmxNCNA-Xfm_DVIaQIoFo1VTCG989DebyNoLc5RlF02nZqLWAUx58B3CrdU69f1gi_K82epgpFMYAsrWVeQMIhI-oqdc5BajTzZAq34R5uUA4UHG4i3fRbwp9_2jebCcSezrGLomoOBdq_U7wabyNqR2iZNNcLeyIMJ4L'
+  let token = 'BQBzGI3PnMyQAcPgoITfq4vc-t4nzB2xAFx7s94xyopEfnIs5w09gR2SGYk01hkr_vZckJLGwc8lVgRkB-35j20B6B6CvZajTh5qKc9bR-01HvVscin2_lZKFrGag2uo5-5v1ahSTsCH5ehq20aiytXSX6al21bHU6t8XaZQIwKFnl-wYkriFhP9sQEUgDFrDeYLpwZ5cP2Ui6uB2Hfg6NIs0PQQYg'
 
   spotifyApi.setAccessToken(token);
 
@@ -33,26 +37,40 @@ function App() {
     // eslint-disable-next-line
   },[searchText]);
 
+
+
+  useHotkeys('shift+s',() => setModalOpen(!modalOpen));
+  useHotkeys('esc',()=>setModalOpen(false))
+
   return (
-      <div className="App">
+      <div className="App" >
         <SpotifyPlayer
           uri={track || "spotify:track:6ozxplTAjWO0BlUxN8ia0A"}
           size = {size}
         />
+        <button onClick={()=>setModalOpen(true)}>Search or Shift + S</button>
+      <Modal
+        isOpen={modalOpen}
+        className="modal"
+        onClick={()=>setModalOpen(true)}
+
+      >
         <div className='RecBox'>
-          <input type="text" onChange={(e) => setSearchText(e.target.value)}/>
           <div className='RecBtns'>
+          <button onClick={()=>setModalOpen(false)}>close</button>
+          <input key="input" type="text" onChange={(e) => setSearchText(e.target.value)} />
             {
               tracks.map((item,i) => {
                 return (
                   <>
-                    <button className="btn" key={item.uri} onClick={(e) => setTrack(e.target.value)}  value={item.uri} >{item.name}</button>
+                    <button className="btn" key={item.uri} onClick={(e) => {setTrack(e.target.value);setModalOpen(false)}}  value={item.uri} >{item.name}</button>
                   </>
                 )
               })
             }
           </div>
         </div>
+        </Modal>
       </div>
   );
 
